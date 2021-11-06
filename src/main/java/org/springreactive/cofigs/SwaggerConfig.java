5e6d8d25e093b8c93be21ed7716ccc6b969473c6
vikas_ver <vikas.ver@ascendcorp.com>
2021-11-06 18:54:13 +0700

    cleaup

--- src/main/java/org/springreactive/cofigs/SwaggerConfig.java
+++ src/main/java/org/springreactive/cofigs/SwaggerConfig.java
@@ -4,22 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 
 @Configuration
 @EnableSwagger2
-@EnableOpenApi
 public class SwaggerConfig {
 
   private ApiInfo apiInfo() {
     return new ApiInfoBuilder()
-        .title("Reactive Documentation")
+        .title("Reactive API")
         .description("Reactive API Documentation")
         .version("1.0.0")
         .build();
